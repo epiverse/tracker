3c8344e07d7c69903fac8218a39a881249a4a82c
Jonas Almeida <jalmeida@mathbiol.org>
2023-01-31 16:59:19 -0500

    require redirected

--- tracker.js
+++ tracker.js
@@ -1,8 +1,9 @@
 console.log('epiVerseTracker imported from https://epiverse.github.io/tracker/export.js');
 
 (async function(){
-    epiVerseTracker = await import('./export.js')
+    //epiVerseTracker = await import('http://localhost:8000/tracker/export.js')
+    epiVerseTracker = await import('https://epiverse.github.io/tracker/export.js')
     if(typeof(define)!='undefined'){
-        define(epiTracker)
+        define(epiVerseTracker)
     }
 })()
