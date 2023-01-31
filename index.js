81a18bf68503f3ddc931bf985f47c097c5262ab9
Jonas Almeida <jalmeida@mathbiol.org>
2023-01-31 16:47:08 -0500

    iarc ini

---
+++ index.js
@@ -0,0 +1,4 @@
+// UI manipulation
+console.log('UI fun')
+
+epiVerseTrackerDiv.innerHTML=':-)'
--- tracker.js
+++ tracker.js
@@ -1,2 +1,8 @@
-console.log('epiVerseTracker')
+console.log('epiVerseTracker imported from https://epiverse.github.io/tracker/export.js');
 
+(async function(){
+    epiVerseTracker = await import('./export.js')
+    if(typeof(define)!='undefined'){
+        define(epiTracker)
+    }
+})()
